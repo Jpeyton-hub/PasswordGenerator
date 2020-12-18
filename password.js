29891d76f6b0dc9a367a05b8390a81b308ec3663
Jpeyton <jpwalk319@gmail.com>
2020-12-18 14:45:03 -0700

    added function and event listner to javascript for generation of password

--- password.js
+++ password.js
@@ -4,10 +4,11 @@ var nums = confirm("Include numbers?");
 var symbols = confirm("Include symbols?");
 var litLet = "abcdefghijklmnopqrstuvwxyz";
 var bigLet = litLet.toUpperCase();
-var possnums = "0123456789"
-var posssymbs = `!'()$%&*+,-./:;<=>?@[\]^_{|}~"`
-var passLength = parseInt(prompt("How many Characters?"))
-var possChar = ""
+var possnums = "0123456789";
+var posssymbs = `!'()$%&*+,-./:;<=>?@[\]^_{|}~"`;
+var passLength = parseInt(prompt("How many Characters?"));
+var possChar = "";
+var button = document.querySelector('#genbtn');
 
 if (lowerCase) {
     possChar += litLet;
@@ -28,4 +29,16 @@ if (passLength > 128 || passLength < 8) {
     alert('Please refresh and enter number of characters between 8 and 128')
 }
 
+function generate() {
+    var result = "";
+    for (var i = 0; i < passLength; i++) {
+        result += possChar.charAt(Math.floor(Math.random() * possChar.length)) 
+    };
+    document.getElementById('cardmsg').textContent = result;    
+} 
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    generate();
+});
      
